49eda7bc40235d5bca8d6085e56fe81688946717
joe-michel <joe.michel.f@gmail.com>
2019-04-05 10:40:05 +0200

    design

--- resources/js/components/CarouselAction.js
+++ resources/js/components/CarouselAction.js
@@ -35,21 +35,11 @@ return (
 		>
 		{events.map(events => (
 		<Carousel.Item key={events.title}>
-<<<<<<< HEAD
-		<div className="carouselMask">
-			<p className="carouselTitle text-center">{events.title}</p>
-			<p className="carouselTitle text-center">{events.begin_time}</p>
-	    </div>
-		  <Link to="/event"><img
-		    className="carouselImg d-block w-100 ofc"
-		    src={"data:image;base64,"+events.image}
-		    alt={events.title}
-		  />
-		  </Link>
-=======
+
 		<Link to="/event">
 				<div className="carouselMask">
 					<p className="carouselTitle text-center">{events.title}</p>
+					<p className="carouselTitle text-center">{events.begin_time}</p>
 				</div>
 				<img
 					className="carouselImg d-block w-100 ofc"
@@ -61,7 +51,6 @@ return (
 				
 				</Carousel.Caption>
 			</Link>
->>>>>>> 7d1a67e1919f41e1bade9c9d0f7a604ff3cc79d1
 		</Carousel.Item>
 		))}
 
