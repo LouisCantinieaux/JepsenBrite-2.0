6fa00ec680aaacec76ef6ec85243b74c9416096a
joe-michel <joe.michel.f@gmail.com>
2019-04-10 10:47:21 +0200

    404 page refactoring

--- resources/js/components/Error.js
+++ resources/js/components/Error.js
@@ -12,8 +12,8 @@ export default class Error extends Component {
       <div className="errorPage">
         <p className="oops">Oops...</p>
         <p className="error">Error 404</p>
+        <p className="errorText mt-4">Page not found</p>
         <img className="john mx-auto" src="https://media1.tenor.com/images/a828888852e708d9afaaad06c7f9513f/tenor.gif?itemid=10251428" />
-        <p className="errorText">This page does not exist, or you're not allowed to see it</p>
       </div>
     )
   }
