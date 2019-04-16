784e184d57106d3e92f5e25ab4f922ccd359b59b
Roppe Pauline <roppepauline@gmail.com>
2019-04-16 14:01:50 +0200

    azer

--- resources/views/InviteToEvent.blade.php
+++ resources/views/InviteToEvent.blade.php
@@ -1,9 +1,20 @@
 @component('mail::message')
-# Introduction
 
-The body of your message.
+# Welcome to JepsenBrite !
 
-@component('mail::button', ['url' => ''])
+##{user.id} invites you at the {name.event} event.
+![picture of event](http://example.com/images/example.png)
+
+If you want to participate in this event, please click on the following link :
+{link of event}
+
+We hope to count you among the next participants in our events
+See you soon !
+
+{logo of the website with link to the site}
+
+
+@component('mail::button', ['url' => 'https://jepsenbrite.herokuapp.com'])
 Button Text
 @endcomponent
 
